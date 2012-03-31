5c49d0f170da8233849b35dda83f2f8ce24b8557
vinayv79 <vyasarao.vinay@sc.com>
2012-03-31 15:57:43 +0800

    package created

---
+++ Togetherness-Android/src/com/togetherness/activity/TogethernessActivity.java
@@ -0,0 +1,11 @@
+package com.togetherness.activity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Prashanth
+ * Date: 3/31/12
+ * Time: 3:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TogethernessActivity {
+}
